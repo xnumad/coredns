dd69826411338c54e3df96eb81af9c4faf95f635
xnumad <34810600+xnumad@users.noreply.github.com>
2023-11-13 17:30:28 +0100

    feat: arp plugin

---
+++ plugin/arp/arp.go
@@ -0,0 +1,66 @@
+package arp
+
+import (
+	"context"
+	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/pkg/dnsutil"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
+	"github.com/miekg/dns"
+	"os/exec"
+)
+
+var log = clog.NewWithPlugin("arp")
+
+type Arp struct {
+	Next plugin.Handler
+}
+
+func (a Arp) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
+	qname := state.Name()
+
+	answers := []dns.RR{}
+
+	if state.QType() != dns.TypePTR {
+		return plugin.NextOrFailure(a.Name(), a.Next, ctx, w, r)
+	}
+
+	ipAddr := dnsutil.ExtractAddressFromReverse(qname)
+	log.Debug("PTR for " + ipAddr)
+	hostname, err := ipToMac(ipAddr)
+	if err != nil {
+		return plugin.NextOrFailure(a.Name(), a.Next, ctx, w, r)
+	}
+	answers = a.ptr(qname, 3600, []string{hostname})
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative = true
+	m.Answer = answers
+
+	w.WriteMsg(m)
+	return dns.RcodeSuccess, nil
+}
+
+func ipToMac(ipAddr string) (macAddr string, e error) {
+	out, err := exec.Command("ndisc6", "-q", ipAddr, "wlp2s0" /*TODO don't hardcode interface*/).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func (a Arp) Name() string { return "arp" }
+
+// from hosts.go
+func (a *Arp) ptr(zone string, ttl uint32, names []string) []dns.RR {
+	answers := make([]dns.RR, len(names))
+	for i, n := range names {
+		r := new(dns.PTR)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypePTR, Class: dns.ClassINET, Ttl: ttl}
+		r.Ptr = dns.Fqdn(n)
+		answers[i] = r
+	}
+	return answers
+}
---
+++ plugin/arp/setup.go
@@ -0,0 +1,18 @@
+package arp
+
+import (
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+)
+
+func init() { plugin.Register("arp", setup) }
+
+func setup(c *caddy.Controller) error {
+	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		return Arp{Next: next}
+	})
+
+	return nil
+}
